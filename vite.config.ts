7958c9202f0d50164d42635adb6f8078bc1b7eed
conjLob <53427774+conjLob@users.noreply.github.com>
2024-01-28 18:42:10 +0900

    Change manifest to ts file for type checking

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,21 @@ import devtools from 'solid-devtools/vite';
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
-import manifest from './manifest.json';
+import manifest from './manifest.config.ts';
 
 export default defineConfig({
   plugins: [crx({ manifest }), solid(), devtools()],
   server: {
+    strictPort: true,
     port: 3000,
+    hmr: {
+      // In extension, `import.meta.url` will be `chrome-extension://<ID>/...`.
+      // So the HMR port must be explicitly set to `3000` to match the dev server.
+      clientPort: 3000,
+    },
   },
   preview: {
+    strictPort: true,
     port: 3000,
   },
   build: {
